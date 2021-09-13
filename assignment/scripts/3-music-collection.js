2835f3a8589b00efd05c36df91cdc92ff1fe6f42
Austin Kammerer <austin4191@gmail.com>
2021-09-13 17:47:13 -0500

    stretch goal - search

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -62,3 +62,31 @@ console.log(
   "Testing findByArtist: find The Beatles (expect 0)",
   findByArtist("The Beatles")
 );
+
+// Stretch Goals
+console.log("********  search  ********");
+function search(searchObj) {
+  if (!searchObj || Object.entries(searchObj).length === 0) {
+    return collection;
+  }
+  let searchCollection = [];
+  for (let album of collection) {
+    if (
+      album.artist === searchObj.artist &&
+      album.yearPublished === searchObj.year
+    ) {
+      searchCollection.push(album);
+    }
+  }
+  return searchCollection;
+}
+console.log(
+  "Testing search: TOOL, 2001 (expect 1)",
+  search({ artist: "TOOL", year: 2001 })
+);
+console.log(
+  "Testing search: Metallica, 2001 (expect empty)",
+  search({ artist: "Metallica", year: 2001 })
+);
+console.log("Testing search: empty search obj (expect collection)", search({}));
+console.log("Testing search: no search obj (expect collection)", search());
