06ec4c36dd8ac19c993d199e8454e15c0f7b89df
Austin Kammerer <austin4191@gmail.com>
2021-09-13 16:34:43 -0500

    addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,32 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+console.log("****** addToCollection ******");
+
+let collection = [];
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(album);
+  return album;
+}
+console.log("Adding 'Lateralus'", addToCollection("Lateralus", "TOOL", 2001));
+console.log(
+  "Adding 'Physical Graffiti'",
+  addToCollection("Physical Graffiti", "Led Zeppelin", 1975)
+);
+console.log(
+  "Adding 'Metallica'",
+  addToCollection("Metallica", "Metallica", 1991)
+);
+console.log("Adding 'Fortitude'", addToCollection("Fortitude", "Gojira", 2021));
+console.log(
+  "Adding 'Rumours'",
+  addToCollection("Rumours", "Fleetwood Mac", 1977)
+);
+console.log(
+  "Adding 'Earth Crisis'",
+  addToCollection("Earth Crisis", "Steel Pulse", 1984)
+);
+console.log(collection);
